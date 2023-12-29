388efaf9fe55894df79c955d7cf493e224169fbe
Xander <alexanderdeisseroth@gmail.com>
2023-12-29 11:10:28 -0800

    deleted obsolete functions

--- server/controllers/attendance-cont.js
+++ server/controllers/attendance-cont.js
@@ -34,28 +34,7 @@ const idEntered = async (req, res, next) => {
     }
 }
 
-const login = (req, res, next) => {
-    const id = req.params.id
-    loggedInUsers[id] = Date.now()
-    if (Object.keys(attendance).includes(id)) {
-        res.send(loggedInUsers)
-    } else {
-        res.send("first login")
-    }
-}
 
-const logout = async (req, res, next) => {
-    const id = req.params.id
-    if (Object.keys(loggedInUsers).includes(id)) {
-        timeElapsed = Date.now() - loggedInUsers[id]
-        console.log(timeElapsed)
-        await set(ref(firebase, `/attendance/${id}/hours`), timeElapsed / 3600000)
-        delete loggedInUsers[id]
-        res.send(loggedInUsers)
-    } else {
-        res.send('not logged in')
-    }
-}
 
 const getHours = () => {
     res.send(attendance)
