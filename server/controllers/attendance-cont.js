9ac95a73b6218e2ebc504c930f722c17ef9da3c1
Xander <alexanderdeisseroth@gmail.com>
2024-01-08 11:47:40 -0800

    added change hours function

--- server/controllers/attendance-cont.js
+++ server/controllers/attendance-cont.js
@@ -18,10 +18,10 @@ onValue(ref(firebase, `/attendance`), (snapshot) => {
 
 
 const idEntered = async (req, res, next) => {
-    const endsOfWeeks = {1: 1705190400000, 2: 1705795200000, 3: 1706400000000, 4: 1707004800000, 5: 1707609600000, 6: 1708214400000, 7: 1708819200000, 8: 1709424000000, 9: 1710028800000, 10: 1710633600000, 11: 1711238400000}
+    const endsOfWeeks = { 1: 1705190400000, 2: 1705795200000, 3: 1706400000000, 4: 1707004800000, 5: 1707609600000, 6: 1708214400000, 7: 1708819200000, 8: 1709424000000, 9: 1710028800000, 10: 1710633600000, 11: 1711238400000 }
     var week = 0
-    for (i in Object.keys(endsOfWeeks)){
-        if (endsOfWeeks[i] > Date.now()){
+    for (i in Object.keys(endsOfWeeks)) {
+        if (endsOfWeeks[i] > Date.now()) {
             week = i
             break
         }
@@ -82,9 +82,41 @@ const checkPassword = (req, res, next) => {
 
 }
 
+const getStudentData = (req, res, next) => {
+    const id = req.params.id
+    for (i in attendance.studentData) {
+        if (attendance.studentData[i].studentId == id) {
+            res.send({ ...attendance.studentData[i], hours: attendance[id].hours })
+            return
+        }
+    }
+    res.send("id not found")
+}
+
 const getSubteamHours = (req, res, next) => {
 
     res.send("Hahahaa")
 }
 
-module.exports = { getTableData, idEntered, checkPassword, getSubteamHours }
+const addHours = async (req, res, next) => {
+    const id = req.body.id
+    const hours = parseFloat(req.body.hours)
+
+    const endsOfWeeks = { 1: 1705190400000, 2: 1705795200000, 3: 1706400000000, 4: 1707004800000, 5: 1707609600000, 6: 1708214400000, 7: 1708819200000, 8: 1709424000000, 9: 1710028800000, 10: 1710633600000, 11: 1711238400000 }
+    var week = 0
+    for (i in Object.keys(endsOfWeeks)) {
+        if (endsOfWeeks[i] > Date.now()) {
+            week = i
+            break
+        }
+
+    }
+
+    await set(ref(firebase, `/attendance/${id}/hoursList/${week}`), attendance[id].hours + hours)
+    await set(ref(firebase, `/attendance/${id}/hours`), attendance[id].hours + hours)
+
+    res.send("added")
+
+}
+
+module.exports = { getTableData, idEntered, checkPassword, getSubteamHours, getStudentData, addHours }
--- server/routes/attendance.js
+++ server/routes/attendance.js
@@ -1,5 +1,5 @@
 const express = require('express'); 
-const { getTableData, idEntered, checkPassword, getSubteamHours} = require('../controllers/attendance-cont')
+const { getTableData, idEntered, checkPassword, getSubteamHours, getStudentData, addHours} = require('../controllers/attendance-cont')
 
 const router = express.Router(); 
 
@@ -11,5 +11,9 @@ router.get('/get-table-data', getTableData)
 router.get('/get-subteam-data', getSubteamHours)
 
 router.get('/check-password/:attempt', checkPassword)
+
+router.get('/get-student-data/:id', getStudentData)
+
+router.post('/add-hours', addHours)
 
 module.exports = router;
