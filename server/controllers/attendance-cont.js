ded42099f7c30d4183b169aa3e084da40e06ddc5
Xander <alexanderdeisseroth@gmail.com>
2024-01-09 22:16:09 -0800

    slackbot testing

--- server/controllers/attendance-cont.js
+++ server/controllers/attendance-cont.js
@@ -145,7 +145,15 @@ const correctStudentData = async (req, res, next) => {
 const getHours = (req, res, next) => {
     console.log("Get hours triggered")
     console.log(req.body)
-    res.send("Here are your hours: sike gottem")
+    var hours = 0
+    for (i in Object.keys(attendance)){
+        if(parseInt(attendance[i])){
+            if(attendance[i].fullName.toLowerCase().split().join('') === req.body.user_name){
+                hours = attendance[i].hours
+            }
+        }
+    }
+    res.send("You have spent "+ toString(hours)+" at lab this season.")
 }
 
 module.exports = { getTableData, idEntered, checkPassword, getSubteamHours, getStudentData, addHours, correctStudentData, getHours }
