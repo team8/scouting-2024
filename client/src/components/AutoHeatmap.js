89c589c7b600db97ae1836417f9f4696d2a7ebac
Connor's Pet <108702832+I-ex@users.noreply.github.com>
2024-03-23 09:04:10 -0700

    team heatmap scale finished

--- client/src/components/AutoHeatmap.js
+++ client/src/components/AutoHeatmap.js
@@ -11,10 +11,10 @@ const AutoHeatmap = (props) => {
     const [redAutoMax, setRedAutoMax] = useState(0);
     const [redAutocompleted, setRedAutocompleted] = useState(false);
     const testingMatrix = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9], [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7], [1, 8], [1, 9], [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8], [2, 9], [3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7], [3, 8], [3, 9], [4, 0], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7], [4, 8], [4, 9], [5, 0], [5, 1], [5, 2], [5, 3], [5, 4], [5, 5], [5, 6], [5, 7], [5, 8], [5, 9], [6, 0], [6, 1], [6, 2], [6, 3], [6, 4], [6, 5], [6, 6], [6, 7], [6, 8], [6, 9], [7, 0], [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7], [7, 8], [7, 9], [8, 0], [8, 1], [8, 2], [8, 3], [8, 4], [8, 5], [8, 6], [8, 7], [8, 8], [8, 9], [9, 0], [9, 1], [9, 2], [9, 3], [9, 4], [9, 5], [9, 6], [9, 7], [9, 8], [9, 9]];
-    const offset_x = 5.2;
-    const offset_y = 14;
-    const multip_x = 4.9;
-    const multip_y = 8.5;
+    const offset_x = 5.4;
+    const offset_y = 7.6;
+    const multip_x = 4.87;
+    const multip_y = 9.15;
 
     useEffect( () => {
         setCoordinatesList(props.coordinatesList);
@@ -43,23 +43,19 @@ const AutoHeatmap = (props) => {
 
     return (
         <Card>
-            <Space h="md"/>
-            <Title order={3}>{props.title}</Title>
-            <Space h="md"/>
-            <Space h="md"/>
-            <div id="mydiv" className="heatmap" >
-                <img src={Field} resizeMode={'cover'} style={{width: '100%'}}/>
-                    {coordinatesList && redAutocompleted && testingMatrix.map((coords, index) => (
+            <div id="mydiv" className="container" style={{padding: '0.1px', overflow: 'none'}}>
+                <img src={Field} resizeMode={'contain'} style={{width: '100%'}}/>
+                    {coordinatesList && redAutocompleted && coordinatesList.map((coords, index) => (
                         <div 
                             key={index}
                             style={{
                                 position: 'absolute',
-                                left: (Number(coords[0]) * 4.8 + 6.1 + '%'),
-                                top: (Number(coords[1]) * 6.7 + 25.5 + '%'),
+                                left: (Number(coords[0]) * multip_x + offset_x + '%'),
+                                top: (Number(coords[1]) * multip_y + offset_y + '%'),
                                 backgroundColor: 'rgb(' + (55+200*(redAutoCoords[coords[0]][coords[1]]/redAutoMax)) + ', 0, 0)',
                                 width: 10,
                                 height: 10,
-                                borderRadius: 50
+                                borderRadius: 50,
                             }}
                         />
                     ))}
