c1164d1713cc4a9fb0dfde5fb4b9fea9fa3531d3
Connor's Pet <108702832+I-ex@users.noreply.github.com>
2024-03-23 07:50:35 -0700

    fixed scaling

--- client/src/components/screens/Match.js
+++ client/src/components/screens/Match.js
@@ -17,6 +17,11 @@ const Match = (props) => {
     const [blues, setBlues] = useState();
     const [redStats, setRedStats] = useState();
     const [blueStats, setBlueStats] = useState();
+    const testingMatrix = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9], [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7], [1, 8], [1, 9], [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8], [2, 9], [3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7], [3, 8], [3, 9], [4, 0], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7], [4, 8], [4, 9], [5, 0], [5, 1], [5, 2], [5, 3], [5, 4], [5, 5], [5, 6], [5, 7], [5, 8], [5, 9], [6, 0], [6, 1], [6, 2], [6, 3], [6, 4], [6, 5], [6, 6], [6, 7], [6, 8], [6, 9], [7, 0], [7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7], [7, 8], [7, 9], [8, 0], [8, 1], [8, 2], [8, 3], [8, 4], [8, 5], [8, 6], [8, 7], [8, 8], [8, 9], [9, 0], [9, 1], [9, 2], [9, 3], [9, 4], [9, 5], [9, 6], [9, 7], [9, 8], [9, 9]];
+    const offset_x = 5.2;
+    const offset_y = 14;
+    const multip_x = 4.9;
+    const multip_y = 8.5;
 
     useEffect(() => {
         setLoading(true);
@@ -205,13 +210,13 @@ const Match = (props) => {
                             <div>
                                 <img src={Field} resizeMode={'cover'} style={{width: '100%'}}/>
                                 {redStats && redAutocompleted && redStats.map((team) => (
-                                    team.qm[number] && team.qm[number].autoCoordinatesList && team.qm[number].autoCoordinatesList.map((coords, index) => (
+                                    team.qm[number] && team.qm[number].autoCoordinatesList && testingMatrix.map((coords, index) => (
                                         <div 
                                             key={index}
                                             style={{
                                                 position: 'absolute',
-                                                left: (Number(coords[0]) * 4.8 + 6.1 + '%'),
-                                                top: (Number(coords[1]) * 6.7 + 25.5 + '%'),
+                                                left: (Number(coords[0]) * multip_x + offset_x + '%'),
+                                                top: (Number(coords[1]) * multip_y + offset_y + '%'),
                                                 backgroundColor: 'rgb(' + (55+200*(redAutoCoords[coords[0]][coords[1]]/redAutoMax)) + ', 0, 0)',
                                                 width: 10,
                                                 height: 10,
@@ -221,13 +226,13 @@ const Match = (props) => {
                                     ))
                                 ))}
                                 {blueStats && blueAutocompleted && blueStats.map((team) => (
-                                    team.qm[number] && team.qm[number].autoCoordinatesList && team.qm[number].autoCoordinatesList.map((coords, index) => (
+                                    team.qm[number] && team.qm[number].autoCoordinatesList && testingMatrix.map((coords, index) => (
                                         <div 
                                             key={index}
                                             style={{
                                                 position: 'absolute',
-                                                left: ((9+Number(coords[0])) * 4.8 + 6.1 + '%'),
-                                                top: (Number(coords[1]) * 6.7 + 25.5 + '%'),
+                                                left: ((9+Number(coords[0])) * multip_x + offset_x + '%'),
+                                                top: (Number(coords[1]) * multip_y + offset_y + '%'),
                                                 backgroundColor: 'rgb(0, 0, ' + (55+200*(blueAutoCoords[coords[0]][coords[1]]/blueAutoMax)) + ')',
                                                 width: 10,
                                                 height: 10,
@@ -248,13 +253,13 @@ const Match = (props) => {
                         <div>
                             <img src={Field} resizeMode={'cover'} style={{width: '100%'}}/>
                             {redStats && redTeleopcompleted && redStats.map((team) => (
-                                    team.qm[number] && team.qm[number].teleopCoordinatesList && team.qm[number].teleopCoordinatesList.map((coords, index) => (
+                                    team.qm[number] && team.qm[number].teleopCoordinatesList && testingMatrix.map((coords, index) => (
                                         <div 
                                             key={index}
                                             style={{
                                                 position: 'absolute',
-                                                left: (Number(coords[0]) * 4.8 + 6.1 + '%'),
-                                                top: (Number(coords[1]) * 6.7 + 25.5 + '%'),
+                                                left: (Number(coords[0]) * multip_x + offset_x + '%'),
+                                                top: (Number(coords[1]) * multip_y + offset_y + '%'),
                                                 backgroundColor: 'rgb(' + (55+200*(redTeleopCoords[coords[0]][coords[1]]/redTeleopMax)) + ', 0, 0)',
                                                 width: 10,
                                                 height: 10,
@@ -264,13 +269,13 @@ const Match = (props) => {
                                     ))
                                 ))}
                             {blueStats && blueTeleopcompleted && blueStats.map((team) => (
-                                    team.qm[number] && team.qm[number].teleopCoordinatesList && team.qm[number].teleopCoordinatesList.map((coords, index) => (
+                                    team.qm[number] && team.qm[number].teleopCoordinatesList && testingMatrix.map((coords, index) => (
                                         <div 
                                             key={index}
                                             style={{
                                                 position: 'absolute',
-                                                left: ((9+Number(coords[0])) * 4.8 + 6.1 + '%'),
-                                                top: (Number(coords[1]) * 6.7 + 25.5 + '%'),
+                                                left: ((9+Number(coords[0])) * multip_x + offset_x + '%'),
+                                                top: (Number(coords[1]) * multip_y + offset_y + '%'),
                                                 backgroundColor: 'rgb(0, 0, ' + (55+200*(blueTeleopCoords[coords[0]][coords[1]]/blueTeleopMax)) + ')',
                                                 width: 10,
                                                 height: 10,
